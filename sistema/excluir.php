ad1780b5355d4904b62eee74c5df4efe7a4f523a
Izaque IFSC <izaque.cr@aluno.ifsc.edu.br>
2025-12-11 15:10:16 -0300

    Update excluir.php

--- sistema/excluir.php
+++ sistema/excluir.php
@@ -2,11 +2,14 @@
     include 'conexao.php';
     $id = $_POST['btnExcluir'];
 
-    // APAGAR produto-caracteristica SE EXISTIR
-    $sql = $pdo->prepare("DELETE FROM Produto_caracteristica WHERE id_produto = ?");
+    // É importante deletar primeiro os registros que dependem de Produto
+    // (Produto_Caracteristica e Estoque) devido às chaves estrangeiras.
+
+    // APAGAR produto-caracteristica
+    $sql = $pdo->prepare("DELETE FROM Produto_Caracteristica WHERE id_produto = ?");
     $sql->execute([$id]);
 
-    // APAGAR estoque SE EXISTIR
+    // APAGAR estoque
     $sql = $pdo->prepare("DELETE FROM Estoque WHERE id_produto = ?");
     $sql->execute([$id]);
 
